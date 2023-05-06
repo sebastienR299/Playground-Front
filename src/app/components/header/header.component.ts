d117f76e828223f2e4c62729dcc83b7386173451
Sebastien <sebastien.a.1990@gmail.com>
2023-05-06 03:27:46 +0200

    Dashboard construction part 1

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, Inject, Renderer2} from '@angular/core';
+import {DOCUMENT} from "@angular/common";
 
 @Component({
   selector: 'app-header',
@@ -7,6 +8,22 @@ import { Component } from '@angular/core';
 })
 export class HeaderComponent {
 
-    public username = "John Doe"
+    constructor(@Inject(DOCUMENT) private document: Document) {
+    }
+
+    public isOpen = true;
+
+    public value = ""
+
+    public toggleSidebar() {
+        if(this.isOpen) {
+            this.isOpen = !this.isOpen;
+            this.document.body.style.setProperty('--sidebar-width', '0');
+        } else {
+            this.isOpen = !this.isOpen;
+            this.document.body.style.setProperty('--sidebar-width', '12rem');
+        }
+    }
+
 
 }
--- src/app/modules/primeng.module.ts
+++ src/app/modules/primeng.module.ts
@@ -1,10 +1,20 @@
 import {NgModule} from '@angular/core';
 import {ButtonModule} from "primeng/button";
 import {TooltipModule} from "primeng/tooltip";
+import {RippleModule} from "primeng/ripple";
+import {InputTextModule} from "primeng/inputtext";
+import {AvatarModule} from "primeng/avatar";
+import {DropdownModule} from "primeng/dropdown";
+import {StyleClassModule} from "primeng/styleclass";
 
 const PRIME = [
     ButtonModule,
-    TooltipModule
+    TooltipModule,
+    RippleModule,
+    InputTextModule,
+    AvatarModule,
+    DropdownModule,
+    StyleClassModule
 ]
 
 @NgModule({
